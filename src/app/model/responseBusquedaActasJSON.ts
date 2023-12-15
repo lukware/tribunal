52e0f326d71e8c7aaa56f31fca3e3bb38712c9ca
Lucas Garcia <l.garcia@santafeciudad.gov.ar>
2023-12-15 08:22:24 -0300

    Refactor Response

--- src/app/model/responseBusquedaActasJSON.ts
+++ src/app/model/responseBusquedaActasJSON.ts
@@ -1,5 +1,4 @@
 import { Acta } from "./acta";
-import { Infraccion } from "./infraccion";
 
 export interface ResponseBusquedaActasJSON {
     Ok:                boolean;
@@ -12,31 +11,3 @@ export interface ResponseBusquedaActasJSON {
     TipoBusqueda:      number;
     Elementos:         Acta[]; //Cambio por Elemento
 }
-
-//Cambio por Elemento
-/*export interface Acta {
-    idActa:            string;
-    codInfraccion1:    number;
-    codInfraccion2:    number;
-    codInfraccion3:    number;
-    codInfraccion4:    number;
-    codInfraccion5:    number;
-    dominio:           string;
-    documento:         string;
-    observacion:       null | string;
-    fecha:             Date;
-    descripcionEstado: string;
-    pagoExtrajudicial: number;
-    liquidable:        number;
-    grave:             number;
-    montoNotificacion: number;
-    actaSugit:         string;
-    codEstado:         number;
-    es_extrania_juris: number;
-    descuento:         number;
-    codNotificacion:   boolean;
-    infracciones:      Infraccion[]; //Se usa la clase Infracion en ves de la interface Infraccion
-    monto:             number;
-    fotomulta:         boolean;
-    montoSinSellado:   number;
-}*/
